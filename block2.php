080af9d210048642897c69d0be9086c06e85a293
Zhangjianfeng <45763349@qq.com>
2017-10-11 10:49:01 +0800

    new file

--- block2.php
+++ block2.php
@@ -77,36 +77,38 @@ function getkey($sock){
 	}
 	return false;
 }
+//消息内容解码
 function decode($buffer) {
-        $len = $masks = $data = $decoded = null;
-        $len = ord($buffer[1]) & 127;
-        if ($len === 126) {
-            $masks = substr($buffer, 4, 4);
-            $data = substr($buffer, 8);
-        }
-        else if ($len === 127) {
-            $masks = substr($buffer, 10, 4);
-            $data = substr($buffer, 14);
-        }
-        else {
-            $masks = substr($buffer, 2, 4);
-            $data = substr($buffer, 6);
-        }
-        //
-        for ($index = 0; $index < strlen($data); $index++) {
-            $decoded .= $data[$index] ^ $masks[$index % 4];
-        }
-        return $decoded;
-    }//解码其实我也没看太懂，但是使用起来是没有任何问题的
-	function encode($s){
-        $a = str_split($s, 125);
-        //添加头文件信息，不然前台无法接受
-        if (count($a) == 1){
-            return "\x81" . chr(strlen($a[0])) . $a[0];
-        }
-        $ns = "";
-        foreach ($a as $o){
-            $ns .= "\x81" . chr(strlen($o)) . $o;
-        }
-        return $ns;
-    }
+	$len = $masks = $data = $decoded = null;
+	$len = ord($buffer[1]) & 127;
+	if ($len === 126) {
+		$masks = substr($buffer, 4, 4);
+		$data = substr($buffer, 8);
+	}
+	else if ($len === 127) {
+		$masks = substr($buffer, 10, 4);
+		$data = substr($buffer, 14);
+	}
+	else {
+		$masks = substr($buffer, 2, 4);
+		$data = substr($buffer, 6);
+	}
+	//
+	for ($index = 0; $index < strlen($data); $index++) {
+		$decoded .= $data[$index] ^ $masks[$index % 4];
+	}
+	return $decoded;
+}
+//消息内容编码
+function encode($s){
+	$a = str_split($s, 125);
+	//添加头文件信息，不然前台无法接受
+	if (count($a) == 1){
+		return "\x81" . chr(strlen($a[0])) . $a[0];
+	}
+	$ns = "";
+	foreach ($a as $o){
+		$ns .= "\x81" . chr(strlen($o)) . $o;
+	}
+	return $ns;
+}
